fe6c14d45a09b61845eef7707d1a9a6908c44bff
Sam Allen <b5026438@my.shu.ac.uk>
2019-04-23 17:24:43 +0100

    Updated the product, category and review classes

--- reviewApp/reviews/models.py
+++ reviewApp/reviews/models.py
@@ -3,22 +3,22 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(default = '', max_length = 255)
+	description = models.TextField()
 
 class Product(models.Model):
 	name = models.CharField(default = '', max_length = 255)
 	brand = models.CharField(default = '', max_length = 255)
-	avg_cost = models.DecimalField(default = 00.00, max_length = 255)
+	avg_cost = models.DecimalField(default = 00.00, max_digits = 4, decimal_places = 2)
 	category = models.ForeignKey(Category, on_delete = models.CASCADE)
 	date_released = models.DateField(default = timezone.now)
 	description = models.TextField()
 	image = models.ImageField(default = 'default.jpg', upload_to = 'product_pics')
 
-class Category(models.Model):
-	name = models.CharField(default = '', max_length = 255)
-	description = models.TextField()
-
 class Review(models.Model):
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
 	product = models.ForeignKey(Product, on_delete = models.CASCADE)
+	rating = models.IntegerField(default = 0, max_length = 5)
 	review_text = models.TextField()
 	date = models.DateField(default = timezone.now)
