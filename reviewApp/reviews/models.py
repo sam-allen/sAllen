1e3dd9beea67ca1b99ad458bc9a30d2685fc8549
Sam Allen <b5026438@my.shu.ac.uk>
2019-04-23 17:58:47 +0100

    Created test data and set up the page displays

--- reviewApp/reviews/models.py
+++ reviewApp/reviews/models.py
@@ -7,14 +7,20 @@ class Category(models.Model):
 	name = models.CharField(default = '', max_length = 255)
 	description = models.TextField()
 
+	def __str__(self):
+		return self.name
+
 class Product(models.Model):
 	name = models.CharField(default = '', max_length = 255)
 	brand = models.CharField(default = '', max_length = 255)
-	avg_cost = models.DecimalField(default = 00.00, max_digits = 4, decimal_places = 2)
+	avg_cost = models.DecimalField(default = 000.00, max_digits = 5, decimal_places = 2)
 	category = models.ForeignKey(Category, on_delete = models.CASCADE)
 	date_released = models.DateField(default = timezone.now)
 	description = models.TextField()
 	image = models.ImageField(default = 'default.jpg', upload_to = 'product_pics')
+
+	def __str__(self):
+		return self.name
 
 class Review(models.Model):
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
@@ -22,3 +28,6 @@ class Review(models.Model):
 	rating = models.IntegerField(default = 0, max_length = 5)
 	review_text = models.TextField()
 	date = models.DateField(default = timezone.now)
+
+	def __str__(self):
+		return f'This is a Review of {self.product.name} by {self.author.profile.full_name}'
